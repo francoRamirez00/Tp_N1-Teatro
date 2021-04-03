f33bf543b38c2ccb7a9c6aaf2161082a122bb77d
FrancoRamirez00 <franmat2011@hotmail.com>
2021-04-03 18:44:51 -0300

    Initial commit

---
+++ TestTeatro.php
@@ -0,0 +1,109 @@
+<?php
+include_once 'teatro.php' ;
+
+
+/* echo "ingrese nombre del teatro : " ;
+$nombreTeatro = trim(fgets(STDIN));
+echo "ingrese direccion del teatro : ". "\n" ;
+$direccionTeatro = trim(fgets(STDIN)) ; */
+$datos= new Teatro("san", "calle 1") ;
+
+ do {
+
+
+    echo " /--- bienvenido al menu ---\ ". "\n" ;
+    echo "elija una opcion : " . "\n" ;
+    echo "1) cambiar nombre del teatro "."\n" ;
+    echo "2) cambiar direccion del teatro". "\n" ;
+    echo "3) camabiar el nombre de una funcion" . "\n" ;
+    echo "4) cambiar el precio de una funcion". "\n" ;
+    echo "5) mostrar informacion del teatro "."\n" ;
+    echo "6) salir del menu ". "\n" ;
+    $eleccion = trim(fgets(STDIN)) ;
+    
+    switch ($eleccion) {
+        case '1':
+
+            echo "ingrese nuevo nombre de teatro : "  ;
+            $nuevoNombre_Teatro = trim(fgets(STDIN)) ;
+            echo "Cargando nombre del teatro .   .   .  . ". "\n" ;
+            $validez = $datos->cambiar_nom_teatro($nuevoNombre_Teatro) ;
+                if ($validez == true) {
+                    echo "El nombre del teatro fue cambiado exitosamente". "\n" ;
+                }else {
+                    echo "el nombre que se ingreso ya existia ". "\n" ;
+    }   
+            echo "saliendo al menu . . .". "\n" ;
+            break;
+        case '2' :
+
+            echo "ingrese nueva direccion : " ;
+            $nuevoNombre_Direccion = trim(fgets(STDIN)) ;
+            echo "Cargando direccion del teatro .   .   .  . ". "\n" ;
+            $validez = $datos->cambiar_nom_Direccion($nuevoNombre_Direccion);
+                if ($validez == true) {
+                        echo "La direccion del teatro fue cambiada exitosamente". "\n" ;
+        
+                }else {
+                        echo "el nombre ingresado ya se encuentra" ."\n" ;
+                        echo "saliendo al menu . . .". "\n" ;
+    }
+           
+            break ;
+        
+        case '3' :
+
+            echo "ingrese nombre de la nueva funcion: " ;
+            $nombreNuevo_Funcion = trim(fgets(STDIN)) ;
+            echo "ingrese que numero de la funcion desea cambiar (0 - 3): " ;
+            $posicion = trim(fgets(STDIN));
+            echo "cargando nombre a la nueva funcion . . . .". "\n" ;
+            $validez = $datos->cambiar_nom_Funcion($posicion,$nombreNuevo_Funcion) ;
+
+                if ($validez) {
+                        echo "El nombre de la funcion ". $posicion. " fue actualizado correctamente" ."\n";
+                }else {
+                        echo "El numero de la posicion ingresado es incorrecto " . "\n";
+}
+                        echo "saliendo al menu . . ." . "\n";
+        break;
+
+        case '4' :
+
+            echo "ingrese el numero de la funcion desea cambiar el precio (0 - 3): " ;
+            $posicion = trim(fgets(STDIN));
+            echo "ingrese el precio que desea cargar: ". "\n" ;
+            $precioNuevo = trim(fgets(STDIN)) ;
+            echo "cargando precio en la posicion " . $posicion. "\n" ;
+            $validez = $datos->cambiar_precio_Funcion($posicion,$precioNuevo) ;
+
+                if ($validez) {
+                    echo "El precio fue actualizado exitosamente ". "\n" ;
+                }else {
+                    echo "numero de la posicion ingresado incorrecto" ;
+}           
+                    echo "saliendo al menu . . ." . "\n";
+    
+        break;
+
+        case '5':
+            
+            echo "Nombre del Teatro: ". $datos->getNombreTeatro() . "\n";
+            echo "direccion del Teatro : ". $datos->getDireccionTeatro() ."\n";
+          
+
+        break;
+
+        case '6' :
+
+            echo "--- gracias por usar el menu ---". "\n" ;
+
+        break;
+    }
+    
+    
+    
+    
+    
+    
+} while ($eleccion < 6 ); 
---
+++ teatro.php
@@ -0,0 +1,139 @@
+<?php
+
+class Teatro{
+
+    private $nombreTeatro ;
+    private $direccionTeatro ;
+    private $funciones_Teatro ;
+
+    public function __construct($nombreTeatro,$direccionTeatro){
+
+        $this->nombreTeatro =$nombreTeatro ;
+        $this->direccionTeatro =$direccionTeatro;
+        $funciones_Teatro =array() ;
+     
+
+        $this->funciones_Teatro = array();
+            $funciones_Teatro[0]=array(
+                                            "nombre" => ["nombre_F0"],"precio" => [150] );
+            $funciones_Teatro[1]=array(
+                                            "nombre" => ["nombre_F1"],"precio" => [200] );
+            $funciones_Teatro[2]=array(
+                                            "nombre" => ["nombre_F2"],"precio" => [250] );
+            $funciones_Teatro[3]=array(
+                                            "nombre" => ["nombre_F3"],"precio" => [300] );
+            
+        
+    
+
+            
+    }
+
+    public function getNombreTeatro(){
+
+        return $this->nombreTeatro ;
+    }
+    public function getDireccionTeatro(){
+
+        return $this->direccionTeatro ;
+    }
+    public function setNombreTeatro($nombreTeatro){
+
+        return $this->nombreTeatro =$nombreTeatro;
+    }
+    public function setDireccionTeatro($direccionTeatro){
+
+        return $this->direccionTeatro =$direccionTeatro ;
+    }
+
+    // funcion para cambiar el nombre al teatro
+    public function cambiar_nom_teatro($nuevoNombre_Teatro){
+
+        if ($nuevoNombre_Teatro !== $this-> getNombreTeatro() ) {
+           
+            $valido = true ;
+            $this->setNombreTeatro($nuevoNombre_Teatro);
+        }else {
+            $valido = false ;
+        }
+
+        return $valido ;
+    }
+
+    // funcion para cambiar la direccion del teatro
+    public function cambiar_nom_Direccion($nuevoNombre_Direccion){
+
+        if ($nuevoNombre_Direccion !== $this-> getDireccionTeatro() ) {
+           
+            $valido = true ;
+            $this->setDireccionTeatro($nuevoNombre_Direccion);
+        }else {
+            $valido = false ;
+        }
+
+        return $valido ;
+    }
+
+    // funcion para cambiar el nombre de la funcion
+    public function cambiar_nom_Funcion($posicion , $nuevoNombre_funcion){
+
+//$nombreDeLaFuncion = $this->funciones_Teatro ;
+    
+            
+                if ($posicion >= 0 && $posicion <4) {
+                    $existe = true ;
+                    $this->funciones_Teatro[$posicion]["nombre"] = $nuevoNombre_funcion ;
+                  
+                }else {
+                    
+                   // $this->funciones_Teatro[$posicion]["nombre"] = $nuevoNombre_funcion ;
+                    $existe = false ;
+                }
+          
+
+            
+
+             return $existe ;
+               
+}
+
+    
+
+
+            // funcion para cambiar el precio de una funcion
+       
+    public function cambiar_precio_funcion($posicion, $precio){
+
+        if ($posicion >= 0 && $posicion <4) {
+            $existe = true ;
+            $this->funciones_Teatro[$posicion]["precio"] =$precio ;
+          
+        }else {
+            
+           // $this->funciones_Teatro[$posicion]["nombre"] = $nuevoNombre_funcion ;
+            $existe = false ;
+        }
+  
+
+    
+
+     return $existe ;
+    
+    }
+
+    
+}
+
+
+
+
+
+
+
+
+ 
+
+ 
+
+
+  
